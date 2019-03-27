fe1bd669d7b2187449e29ee2e093b91c8ec75bd1
Vinay <vinaysinghsatna01@gmail.com>
2019-03-28 01:26:17 +0530

    Update validations

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,8 +1,9 @@
 
 export const isEmailValid = (email) => {
   const emailPattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (!emailPattern.test(email)) {
-    return true;
-  }
-  return false;
+  return emailPattern.test(email);
+};
+
+export const isPasswordValid = (password) => {
+  return password.trim() && password.length > 7;
 };
