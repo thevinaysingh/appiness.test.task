0b611dbd6e77ca08d1ef2f96788b5968586989de
Vinay <vinaysinghsatna01@gmail.com>
2019-03-28 01:26:45 +0530

    Add local db

--- src/utils/locales.js
+++ src/utils/locales.js
@@ -2,4 +2,79 @@ export const locales = {
   SPLASH: {
     label: 'Appiness test task',
   },
+  LOGIN: {
+    email: 'Email',
+    password: 'Password',
+    login: 'Login',
+    email_validation_msg: 'Please enter valid email',
+    password_validation_msg: 'Please enter valid password',
+  },
+  DASHBOARD: {
+    title: 'Dashboard',
+    logout: 'Logout',
+    employees_list: 'List of Employees',
+  },
+};
+
+export const LOCAL_DB = {
+  LOGIN: {
+    username: 'prashant@gmail.com',
+    password: 'prashant123',
+  },
+  LOGIN_MSG: {
+    incorrect_email: 'Incorrect email',
+    incorrect_password: 'Incorrect password',
+    login_success: 'Login success!',
+    access_token: new Date().getTime.toString(),
+  },
+  user: [
+    {
+      "id":1,
+      "name":"test1",
+      "age" : "11",
+      "gender":"male",
+      "email" : "test1@gmail.com",
+      "phoneNo" : "9738452245"
+    },
+    {
+      "id" : 2,
+      "name":"test2",
+      "age" : "12",
+      "gender":"male",
+      "email" : "test2@gmail.com",
+      "phoneNo" : "9738452245"
+    },
+    {
+      "id":3,
+      "name":"test3",
+      "age" : "13",
+      "gender":"male",
+      "email" : "test3@gmail.com",
+      "phoneNo" : "9738452245"
+    },
+    {
+      "id":4,
+      "name":"test4",
+      "age" : "14",
+      "gender":"male",
+      "email" : "test4@gmail.com",
+      "phoneNo" : "9738452245"
+    },
+    {
+      "id":5,
+      "name":"test5",
+      "age" : "15",
+      "gender":"male",
+      "email" : "test5@gmail.com",
+      "phoneNo" : "9738452245"
+    },
+    {
+      "id":6,
+      "name":"test6",
+      "age" : "16",
+      "gender":"male",
+      "email" : "test6@gmail.com",
+      "phoneNo" : "9738452245"
+    },
+  ],
 };
